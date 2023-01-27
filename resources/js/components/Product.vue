33662b78dd7ecb5289c30f8668d243df5238f7a5
Nick <nicholas@wealthyplanet.com>
2023-01-27 18:28:13 -0500

    Begin styling individual product card

--- resources/js/components/Product.vue
+++ resources/js/components/Product.vue
@@ -1,11 +1,15 @@
 <template>
-    <div>
+<!--  Image and product name are wrapped in a-tags, the other pages are not built out but they would like
+  to pages showing the individual information for that product  -->
+    <div class="max-w-[366px] rounded-lg shadow hover:shadow-lg overflow-hidden">
         <!-- productImage -->
-        <img src="https://assets.atdw-online.com.au/images/12e14fafc353e8d431cb9c0f05f1d0d7.jpeg?rect=0%2C0%2C1600%2C1200&w=280&h=210&rot=360&q=eyJ0eXBlIjoibGlzdGluZyIsImxpc3RpbmdJZCI6IjYyOTU3NGViNjg0MTE1N2QzMDYyNjQ5ZiIsImRpc3RyaWJ1dG9ySWQiOiI1NmIxZWI5MzQ0ZmVjYTNkZjJlMzIwYzgiLCJhcGlrZXlJZCI6IjU2YjFlZTU5MGNmMjEzYWQyMGRjNTgxOSJ9">
-        <!-- productName -->
-        <h2>A Day Out' by Marguerite Derricourt</h2>
-        <!-- productDescription -->
-        <p>On 3 July 1999, Rundle Mall welcomed four new residents  Horatio, Oliver, Truffles and Augusta. Made completely from bronze, these life-sized pigs look to be having a great day out in Rundle Mall as they walk the Mall, dig through the bin for food scraps and greet passers-by.\n\nThe art installation officially known as A Day Out was created by Marguerite Derricourt, the winner of a national sculpture competition instigated by the City of Adelaide in the final stages of the Rundle Mall upgrades in the late 1990s.\n\nThe people of Adelaide loved the pigs so much, a competition was held to name them and you can find a plaque next to each pig stating its name and the name of the person who named them.\n\nChildren and adults alike are often seen hugging, sitting on and posing for pictures with the popular pigs on their days out in Rundle Mall.</p>
+        <a href="#"><img class="w-full" src="https://assets.atdw-online.com.au/images/12e14fafc353e8d431cb9c0f05f1d0d7.jpeg?rect=0%2C0%2C1600%2C1200&w=280&h=210&rot=360&q=eyJ0eXBlIjoibGlzdGluZyIsImxpc3RpbmdJZCI6IjYyOTU3NGViNjg0MTE1N2QzMDYyNjQ5ZiIsImRpc3RyaWJ1dG9ySWQiOiI1NmIxZWI5MzQ0ZmVjYTNkZjJlMzIwYzgiLCJhcGlrZXlJZCI6IjU2YjFlZTU5MGNmMjEzYWQyMGRjNTgxOSJ9"></a>
+        <div class="py-8 px-4">
+            <!-- productName -->
+            <a href="#"><h2 class="font-bold text-lg pb-4">A Day Out' by Marguerite Derricourt</h2></a>
+            <!-- productDescription -->
+            <p class="line-clamp-4">On 3 July 1999, Rundle Mall welcomed four new residents  Horatio, Oliver, Truffles and Augusta. Made completely from bronze, these life-sized pigs look to be having a great day out in Rundle Mall as they walk the Mall, dig through the bin for food scraps and greet passers-by.\n\nThe art installation officially known as A Day Out was created by Marguerite Derricourt, the winner of a national sculpture competition instigated by the City of Adelaide in the final stages of the Rundle Mall upgrades in the late 1990s.\n\nThe people of Adelaide loved the pigs so much, a competition was held to name them and you can find a plaque next to each pig stating its name and the name of the person who named them.\n\nChildren and adults alike are often seen hugging, sitting on and posing for pictures with the popular pigs on their days out in Rundle Mall.</p>
+        </div>
     </div>
 </template>
 
