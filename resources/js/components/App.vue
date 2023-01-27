66d158986ab5ca7e136f91af73e26bbc251a5ce9
Nick <nicholas@wealthyplanet.com>
2023-01-27 18:01:09 -0500

    Create Product component with base layout

--- resources/js/components/App.vue
+++ resources/js/components/App.vue
@@ -1,10 +1,13 @@
 <template>
     <h1 class="text-center">Welcome!</h1>
+    <product></product>
 </template>
 
 <script>
+import Product from "./Product.vue";
 export default {
-    name: "App"
+    name: "App",
+    components: {Product}
 }
 </script>
 
