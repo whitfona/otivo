8ffde7383bf4610fd2ae08d5de88dc77f79b6c72
Nick <nicholas@wealthyplanet.com>
2023-01-27 16:42:31 -0500

    Get all products with productCategoryId 'ACCOMM' or 'ATTRACTION'

--- routes/web.php
+++ routes/web.php
@@ -1,5 +1,6 @@
 <?php
 
+use Illuminate\Support\Facades\Http;
 use Illuminate\Support\Facades\Route;
 
 /*
@@ -19,9 +20,15 @@ Route::get('/', function () {
 
 // Get all products with productCategoryId 'ACCOMM' or 'ATTRACTION'
 Route::get('/products', function () {
-    // Get all products from API - https://atlas.atdw-online.com.au/api/atlas/products?key=123456789101112&cats=ACCOMM,ATTRACTION&out=json
+    $API_KEY = env('ATDW_API_KEY');
+    $response = Http::get('https://atlas.atdw-online.com.au/api/atlas/products?key=' . $API_KEY . '&cats=ACCOMM,ATTRACTION&out=json');
 
-    // Send data to frontend
+    if (!$response->successful()) {
+        $response->throw();
+    }
+
+    // will the UTF-16LE encoding cause an error?
+    return response($response->body());
 
     // Handle errors
 });
