a2b61a76d8cd68c6ef98cbb1132266127eb6317c
Nick <nicholas@wealthyplanet.com>
2023-01-28 15:59:59 -0500

    Create base controller with common logic (name could be improved)

---
+++ app/Http/Controllers/BaseController.php
@@ -0,0 +1,28 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use GuzzleHttp\Client;
+
+class BaseController extends Controller
+{
+    public function getData($urlOptions)
+    {
+        $client = new Client();
+        $API_KEY = env('ATDW_API_KEY');
+
+        $response = $client->request(
+            'GET',
+            'https://atlas.atdw-online.com.au/api/atlas/products?key=' . $API_KEY . $urlOptions
+        );
+
+        if (!$response->getBody()) {
+            return response('Error getting products', 500);
+        }
+
+        $content = $response->getBody()->getContents();
+        return json_decode(
+            mb_convert_encoding($content, 'UTF-8', 'UTF-16LE')
+        );
+    }
+}
