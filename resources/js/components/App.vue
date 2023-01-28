531e838ce3faec5867efe2663f36b1cfb8356bcf
Nick <nicholas@wealthyplanet.com>
2023-01-27 22:41:32 -0500

    Get all products from backend

--- resources/js/components/App.vue
+++ resources/js/components/App.vue
@@ -1,13 +1,28 @@
 <template>
     <h1 class="text-center">Welcome!</h1>
-    <product></product>
+    {{products}}
 </template>
 
 <script>
 import Product from "./Product.vue";
 export default {
     name: "App",
-    components: {Product}
+    components: {Product},
+    data() {
+        return {
+            products: [],
+        }
+    },
+    methods: {
+        getProducts() {
+            axios.get('/products')
+                .then(response => this.products = response.data)
+                .catch(error => console.log(error));
+        }
+    },
+    mounted() {
+        this.getProducts()
+    }
 }
 </script>
 
