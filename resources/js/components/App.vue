725130ec517fe41f411d1caf638e3e4b3d088b6d
Nick <nicholas@wealthyplanet.com>
2023-01-30 13:26:06 +1100

    Add error state

--- resources/js/components/App.vue
+++ resources/js/components/App.vue
@@ -1,5 +1,8 @@
 <template>
-    <div class="p-4 max-w-[1200px] mx-auto">
+    <div v-if="isError">
+        <h1 class="text-center text-red-600 text-xl pt-6">Sorry, there was an error getting the products, please try again.</h1>
+    </div>
+    <div v-else class="p-4 max-w-[1200px] mx-auto">
         <div class="px-2 py-4 mb-4 sm:flex gap-8 bg-blue-600">
             <div class="flex flex-col">
                 <label for="regions" class="pb-2 text-white text-xl font-light">Region</label>
@@ -44,7 +47,8 @@ export default {
             regions: [],
             location: '',
             region: '',
-            isLoading: true
+            isLoading: true,
+            isError: false,
         }
     },
     methods: {
@@ -54,21 +58,30 @@ export default {
                     this.products = response.data
                     this.isLoading = false
                 })
-                .catch(error => console.log(error));
+                .catch(error => {
+                    this.isError = true
+                    console.log(error)
+                })
         },
         getLocations() {
             axios.get('/locations')
                 // This was sorted on the backend but is not showing sorted in the UI, I don't want to sort it again
                 // on the front end, not sure on the best approach of sorting on the backend or frontend
                 .then(response => this.locations = response.data)
-                .catch(error => console.log(error));
+                .catch(error => {
+                    this.isError = true
+                    console.log(error)
+                })
         },
         getRegions() {
             axios.get('/regions')
                 // This was sorted on the backend but is not showing sorted in the UI, I don't want to sort it again
                 // on the front end, not sure on the best approach of sorting on the backend or frontend
                 .then(response => this.regions = response.data)
-                .catch(error => console.log(error));
+                .catch(error => {
+                    this.isError = true
+                    console.log(error)
+                })
         },
         onChangeLocation(event) {
             this.location = event.target.value
@@ -83,7 +96,10 @@ export default {
                     this.products = response.data
                     this.isLoading = false
                 })
-                .catch(error => console.log(error))
+                .catch(error => {
+                    this.isError = true
+                    console.log(error)
+                })
         },
         getProductsByRegion() {
             this.isLoading = true
@@ -92,7 +108,10 @@ export default {
                     this.products = response.data
                     this.isLoading = false
                 })
-                .catch(error => console.log(error))
+                .catch(error => {
+                    this.isError = true
+                    console.log(error)
+                })
         }
     },
     mounted() {
